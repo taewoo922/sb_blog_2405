feaa8f0387d1b7e449b4a79fe382648f5cffe341
taewoo922 <kimtaewoo922@gmail.com>
2024-05-23 17:05:39 +0900

    Member 엔티티 등록

---
+++ src/main/java/com/example/blog/domain/member/entity/member.java
@@ -0,0 +1,27 @@
+package com.example.blog.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class member {
+    @Comment("유저 아이디")
+    @Column
+    private String username;
+    private String password;
+    @Column(unique = true)
+    private String nickname;
+    private String email;
+
+}
