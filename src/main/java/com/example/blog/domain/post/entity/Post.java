c625e6ea94bd4066e1d54872aed42f4453f93181
taewoo922 <kimtaewoo922@gmail.com>
2024-05-23 15:36:01 +0900

    entity, post 기본 작업

--- src/main/java/com/example/blog/domain/post/entity/Post.java
+++ src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -1,12 +1,17 @@
 package com.example.blog.domain.post.entity;
 
+import com.example.blog.global.security.jpa.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
-public class Post {
-    @Id
-    @GeneratedValue
-    private Long seq;
+@Getter
+@SuperBuilder
+public class Post extends BaseEntity {
+    private String title;
+    private String content;
 }
