eec906d4877baaa39689179a8dd6260eb11e28cf
taewoo922 <kimtaewoo922@gmail.com>
2024-05-23 17:17:12 +0900

    Member MVC 세팅, Post author 추가가

--- src/main/java/com/example/blog/global/security/SecurityConfig.java
+++ src/main/java/com/example/blog/global/security/SecurityConfig.java
@@ -1,10 +1,4 @@
-package com.example.blog.domain.post.repository;
+package com.example.blog.global.security;
 
-import com.example.blog.domain.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-
+public class SecurityConfig {
 }
