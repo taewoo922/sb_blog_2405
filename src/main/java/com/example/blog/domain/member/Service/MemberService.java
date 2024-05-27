f568f019415adf0c4c27db644180628e6fd11897
taewoo922 <kimtaewoo922@gmail.com>
2024-05-27 16:19:10 +0900

    소셜 로그인 처리 - 카카오

--- src/main/java/com/example/blog/domain/member/Service/MemberService.java
+++ src/main/java/com/example/blog/domain/member/Service/MemberService.java
@@ -5,6 +5,9 @@ import com.example.blog.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +24,19 @@ public class MemberService {
                 .email(email)
                 .build();
         return memberRepository.save(member);
+    }
+    @Transactional
+    public Member whenSocialLogin(String providerTypeCode, String username, String nickname) {
+        Optional<Member> opMember = findByUsername(username);
+
+        if (opMember.isPresent()) return opMember.get();
+
+        // 소셜 로그인를 통한 가입시 비번은 없다.
+        return signup(username, "", nickname, ""); // 최초 로그인 시 딱 한번 실행
+    }
+
+    private Optional<Member> findByUsername(String username) {
+        return memberRepository.findByusername(username);
     }
 
 
--- src/main/java/com/example/blog/global/security/SecurityConfig.java
+++ src/main/java/com/example/blog/global/security/SecurityConfig.java
@@ -27,6 +27,10 @@ public class SecurityConfig {
                                 .loginPage("/member/login")
                                 .defaultSuccessUrl("/")
                 )
+                .oauth2Login(
+                        oauth2Login -> oauth2Login
+                                .loginPage("/member/login")
+                )
                 .logout(
                         logout -> logout
                                 .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
