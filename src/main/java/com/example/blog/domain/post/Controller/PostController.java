a829d4c98c2499c96cc8b7958dfca92457ac28a3
taewoo922 <kimtaewoo922@gmail.com>
2024-05-29 14:35:49 +0900

    게시글 등록 작업

--- src/main/java/com/example/blog/domain/post/Controller/PostController.java
+++ src/main/java/com/example/blog/domain/post/Controller/PostController.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +25,17 @@ public class PostController {
         model.addAttribute("postList", postList);
 
         return "post/list";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "post/create_form";
+    }
+
+    @PostMapping("/create")
+    public String create(@RequestParam("title") String title, @RequestParam("content") String content) {
+        postService.create(title,content);
+
+        return "redirect:/post/list";
     }
 }
